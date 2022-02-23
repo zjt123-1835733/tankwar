949f5235f36d7fcbd8cf5ca8496db7f993c91981
Juntao Zhong <zjt123@uw.edu>
2022-02-22 17:36:32 -0800

    tank move

---
+++ src/main/java/com/javaneversleep/tankwar/Direction.java
@@ -0,0 +1,13 @@
+package com.javaneversleep.tankwar;
+
+public enum Direction {
+
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    UPLEFT,
+    UPRIGHT,
+    DOWNLEFT,
+    DOWNRIGHT;
+}
--- src/main/java/com/javaneversleep/tankwar/GameClient.java
+++ src/main/java/com/javaneversleep/tankwar/GameClient.java
@@ -2,21 +2,22 @@ package com.javaneversleep.tankwar;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class GameClient extends JComponent {
     //构造方法
+    private Tank playerTank;
+
     private GameClient() {
+        this.playerTank = new Tank(400,100, Direction.DOWN);
         this.setPreferredSize(new Dimension(800,600));
     }
 
     //alt + insert to override
     @Override
     protected void paintComponent(Graphics g) {
-        //调用父类
-        //super.paintComponent(g);
-        //相对路径定位文件，正斜杠简化操作
-        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(),
-            400, 100, null);
+        playerTank.draw(g);
     }
 
     public static void main(String[] args) {
@@ -28,7 +29,44 @@ public class GameClient extends JComponent {
         frame.add(client);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack(); //自适应
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                client.playerTank.keyPressed(e);
+            }
+//                switch (e.getKeyCode()) {
+//                    case KeyEvent.VK_UP:
+//                        client.playerTank.setY(client.playerTank.getY() - 5);
+//                        break;
+//                    case KeyEvent.VK_DOWN:
+//                        client.playerTank.setY(client.playerTank.getY() + 5);
+//                        break;
+//                    case KeyEvent.VK_LEFT:
+//                        client.playerTank.setX(client.playerTank.getX() - 5);
+//                        break;
+//                    case KeyEvent.VK_RIGHT:
+//                        client.playerTank.setX(client.playerTank.getX() + 5);
+//                        break;
+//                }
+//                client.playerTank.move();
+//            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                client.playerTank.keyReleased(e);
+            }
+        });
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        //另起一个线程
+        while (true) {
+            client.repaint();
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
